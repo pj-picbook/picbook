879e781a3cdbf76cefa82ba9d7b31f2162789139
tokku5552 <physics.0121@gmail.com>
2022-07-20 23:21:44 +0900

    add: build_runnnerを実行

--- lib/domain/entity/user.dart
+++ lib/domain/entity/user.dart
@@ -1,3 +1,7 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+
 @freezed
 class User with _$User {
   const User._();
---
+++ lib/domain/entity/user.freezed.dart
@@ -0,0 +1,237 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+User _$UserFromJson(Map<String, dynamic> json) {
+  return _User.fromJson(json);
+}
+
+/// @nodoc
+mixin _$User {
+  String get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
+  String get linkedAccount => throw _privateConstructorUsedError;
+  List<String> get bookshelfs => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $UserCopyWith<User> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UserCopyWith<$Res> {
+  factory $UserCopyWith(User value, $Res Function(User) then) =
+      _$UserCopyWithImpl<$Res>;
+  $Res call(
+      {String id,
+      String name,
+      String email,
+      String linkedAccount,
+      List<String> bookshelfs});
+}
+
+/// @nodoc
+class _$UserCopyWithImpl<$Res> implements $UserCopyWith<$Res> {
+  _$UserCopyWithImpl(this._value, this._then);
+
+  final User _value;
+  // ignore: unused_field
+  final $Res Function(User) _then;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? email = freezed,
+    Object? linkedAccount = freezed,
+    Object? bookshelfs = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      linkedAccount: linkedAccount == freezed
+          ? _value.linkedAccount
+          : linkedAccount // ignore: cast_nullable_to_non_nullable
+              as String,
+      bookshelfs: bookshelfs == freezed
+          ? _value.bookshelfs
+          : bookshelfs // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_UserCopyWith<$Res> implements $UserCopyWith<$Res> {
+  factory _$$_UserCopyWith(_$_User value, $Res Function(_$_User) then) =
+      __$$_UserCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String id,
+      String name,
+      String email,
+      String linkedAccount,
+      List<String> bookshelfs});
+}
+
+/// @nodoc
+class __$$_UserCopyWithImpl<$Res> extends _$UserCopyWithImpl<$Res>
+    implements _$$_UserCopyWith<$Res> {
+  __$$_UserCopyWithImpl(_$_User _value, $Res Function(_$_User) _then)
+      : super(_value, (v) => _then(v as _$_User));
+
+  @override
+  _$_User get _value => super._value as _$_User;
+
+  @override
+  $Res call({
+    Object? id = freezed,
+    Object? name = freezed,
+    Object? email = freezed,
+    Object? linkedAccount = freezed,
+    Object? bookshelfs = freezed,
+  }) {
+    return _then(_$_User(
+      id: id == freezed
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: email == freezed
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      linkedAccount: linkedAccount == freezed
+          ? _value.linkedAccount
+          : linkedAccount // ignore: cast_nullable_to_non_nullable
+              as String,
+      bookshelfs: bookshelfs == freezed
+          ? _value._bookshelfs
+          : bookshelfs // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_User extends _User {
+  _$_User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.linkedAccount,
+      required final List<String> bookshelfs})
+      : _bookshelfs = bookshelfs,
+        super._();
+
+  factory _$_User.fromJson(Map<String, dynamic> json) => _$$_UserFromJson(json);
+
+  @override
+  final String id;
+  @override
+  final String name;
+  @override
+  final String email;
+  @override
+  final String linkedAccount;
+  final List<String> _bookshelfs;
+  @override
+  List<String> get bookshelfs {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_bookshelfs);
+  }
+
+  @override
+  String toString() {
+    return 'User(id: $id, name: $name, email: $email, linkedAccount: $linkedAccount, bookshelfs: $bookshelfs)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_User &&
+            const DeepCollectionEquality().equals(other.id, id) &&
+            const DeepCollectionEquality().equals(other.name, name) &&
+            const DeepCollectionEquality().equals(other.email, email) &&
+            const DeepCollectionEquality()
+                .equals(other.linkedAccount, linkedAccount) &&
+            const DeepCollectionEquality()
+                .equals(other._bookshelfs, _bookshelfs));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(id),
+      const DeepCollectionEquality().hash(name),
+      const DeepCollectionEquality().hash(email),
+      const DeepCollectionEquality().hash(linkedAccount),
+      const DeepCollectionEquality().hash(_bookshelfs));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_UserCopyWith<_$_User> get copyWith =>
+      __$$_UserCopyWithImpl<_$_User>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_UserToJson(
+      this,
+    );
+  }
+}
+
+abstract class _User extends User {
+  factory _User(
+      {required final String id,
+      required final String name,
+      required final String email,
+      required final String linkedAccount,
+      required final List<String> bookshelfs}) = _$_User;
+  _User._() : super._();
+
+  factory _User.fromJson(Map<String, dynamic> json) = _$_User.fromJson;
+
+  @override
+  String get id;
+  @override
+  String get name;
+  @override
+  String get email;
+  @override
+  String get linkedAccount;
+  @override
+  List<String> get bookshelfs;
+  @override
+  @JsonKey(ignore: true)
+  _$$_UserCopyWith<_$_User> get copyWith => throw _privateConstructorUsedError;
+}
