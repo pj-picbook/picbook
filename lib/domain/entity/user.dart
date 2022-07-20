21062881893be38b163419b4b7ad2d7e107a9def
tokku5552 <physics.0121@gmail.com>
2022-07-20 23:18:49 +0900

    wip: entityクラスを作成

---
+++ lib/domain/entity/user.dart
@@ -0,0 +1,14 @@
+@freezed
+class User with _$User {
+  const User._();
+
+  factory User(
+      {required String id,
+      required String name,
+      required String email,
+      required String linkedAccount,
+      required List<String> bookshelfs // Bookshelfsが無いので仮
+      }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
