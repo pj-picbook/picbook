5c07bbeb632269681c02d82de4f776ed0d64bc10
Romu1273x <romu1273x@yahoo.co.jp>
2022-07-24 11:19:43 +0900

    Androidのバックボタン対応と微修正

--- lib/presentation/bottom_navigation_page.dart
+++ lib/presentation/bottom_navigation_page.dart
@@ -14,52 +14,68 @@ class BottomNavigationPage extends StatefulWidget {
 }
 
 class _BottomNavigationPageState extends State<BottomNavigationPage> {
-  TabItem currentTab = TabItem.bookshelf;
+  TabItem _currentTab = TabItem.bookshelf;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: TabItem.values
-            .map(
-              (tabItem) => Offstage(
-                offstage: currentTab != tabItem,
-                child: Navigator(
-                  key: _navigatorKeys[tabItem],
-                  onGenerateRoute: (settings) {
-                    return MaterialPageRoute<Widget>(
-                      builder: (context) => tabItem.page,
-                    );
-                  },
-                ),
-              ),
-            )
-            .toList(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: currentTab.index,
-        items: TabItem.values
-            .map(
-              (tabItem) => BottomNavigationBarItem(
-                icon: Icon(tabItem.icon),
-                label: tabItem.title,
-              ),
-            )
-            .toList(),
-        onTap: (index) {
-          final selectedTab = TabItem.values[index];
-          if (currentTab == selectedTab) {
-            _navigatorKeys[selectedTab]
-                ?.currentState
-                ?.popUntil((route) => route.isFirst);
-          } else {
-            setState(() {
-              currentTab = selectedTab;
-            });
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab =
+            !await _navigatorKeys[_currentTab]!.currentState!.maybePop();
+        if (isFirstRouteInCurrentTab) {
+          if (_currentTab != TabItem.bookshelf) {
+            onSelect(TabItem.bookshelf);
+            return false;
           }
-        },
+        }
+        return isFirstRouteInCurrentTab;
+      },
+      child: Scaffold(
+        body: Stack(
+          children: TabItem.values
+              .map(
+                (tabItem) => Offstage(
+                  offstage: _currentTab != tabItem,
+                  child: Navigator(
+                    key: _navigatorKeys[tabItem],
+                    onGenerateRoute: (settings) {
+                      return MaterialPageRoute<Widget>(
+                        builder: (context) => tabItem.page,
+                      );
+                    },
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentTab.index,
+          items: TabItem.values
+              .map(
+                (tabItem) => BottomNavigationBarItem(
+                  icon: Icon(tabItem.icon),
+                  label: tabItem.title,
+                ),
+              )
+              .toList(),
+          onTap: (index) {
+            onSelect(TabItem.values[index]);
+          },
+        ),
       ),
     );
+  }
+
+  void onSelect(TabItem selectedTab) {
+    if (_currentTab == selectedTab) {
+      _navigatorKeys[selectedTab]
+          ?.currentState
+          ?.popUntil((route) => route.isFirst);
+    } else {
+      setState(() {
+        _currentTab = selectedTab;
+      });
+    }
   }
 }
