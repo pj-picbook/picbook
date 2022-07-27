1aaaec796b5d86bdf59a7b782b86a633bad19c47
まさたか <shinonome419@icloud.com>
2022-07-27 18:13:15 +0900

    clean: 不要なボタンの削除、およびコードの整形
    ・Google、Appleでの登録画面が不要になったため、それらの画面に遷移する為のボタンを削除。
    ・Linterにならいconstの付与、およびchildプロパティの位置を変更

--- lib/presentation/first_page/first_page.dart
+++ lib/presentation/first_page/first_page.dart
@@ -11,137 +11,101 @@ class FirstPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          SizedBox(height: 30),
+          const SizedBox(height: 30),
           Container(
             height: 200,
             width: 200,
-            child: Center(
+            decoration: const BoxDecoration(
+              color: Colors.black12,
+            ),
+            child: const Center(
               child: Text(
                 "ロゴ",
                 style: TextStyle(fontWeight: FontWeight.w700),
               ),
             ),
-            decoration: BoxDecoration(
-              color: Colors.black12,
-            ),
           ),
-          SizedBox(height: 10),
-          Text(
+          const SizedBox(height: 10),
+          const Text(
             "アカウントをお持ちの方",
             style: TextStyle(fontWeight: FontWeight.w600),
           ),
           TextButton(
+            onPressed: null,
             child: Container(
               height: 50,
               width: 270,
-              child: Center(
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                border: Border.all(color: Colors.red, width: 0.5),
+              ),
+              child: const Center(
                 child: Text(
                   "ログイン",
                   style:
                       TextStyle(color: Colors.red, fontWeight: FontWeight.w900),
                 ),
               ),
-              decoration: BoxDecoration(
-                color: Colors.white70,
-                border: Border.all(color: Colors.red, width: 0.5),
-              ),
             ),
-            onPressed: null,
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
-          Text(
+          const Text(
             "初めてご利用の方",
             style: TextStyle(fontWeight: FontWeight.w600),
           ),
           TextButton(
+            onPressed: null,
             child: Container(
               height: 50,
               width: 270,
-              child: Center(
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                border: Border.all(color: Colors.red, width: 0.5),
+              ),
+              child: const Center(
                 child: Text(
                   "メールアドレスで登録",
                   style:
                       TextStyle(color: Colors.red, fontWeight: FontWeight.w600),
                 ),
               ),
-              decoration: BoxDecoration(
-                color: Colors.white70,
-                border: Border.all(color: Colors.red, width: 0.5),
-              ),
             ),
-            onPressed: null,
-          ),
-          TextButton(
-            child: Container(
-              height: 50,
-              width: 270,
-              child: Center(
-                child: Text(
-                  "Googleで登録",
-                  style:
-                      TextStyle(color: Colors.red, fontWeight: FontWeight.w600),
-                ),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white70,
-                border: Border.all(color: Colors.red, width: 0.5),
-              ),
-            ),
-            onPressed: null,
-          ),
-          TextButton(
-            child: Container(
-              height: 50,
-              width: 270,
-              child: Center(
-                child: Text(
-                  "Appleでサインアップ",
-                  style:
-                      TextStyle(color: Colors.red, fontWeight: FontWeight.w600),
-                ),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white70,
-                border: Border.all(color: Colors.red, width: 0.5),
-              ),
-            ),
-            onPressed: null,
           ),
           Container(
-            padding: EdgeInsets.all(20),
+            padding: const EdgeInsets.all(20),
             child: RichText(
               text: TextSpan(
-                style: TextStyle(
+                style: const TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.w500,
                 ),
                 children: [
-                  TextSpan(
+                  const TextSpan(
                     text: "登録/ログインをすることで、",
                   ),
                   TextSpan(
                     text: "利用規約",
                     recognizer: TapGestureRecognizer()..onTap = () {},
-                    style: TextStyle(
+                    style: const TextStyle(
                       color: Colors.blue,
                       fontWeight: FontWeight.w600,
                       decoration: TextDecoration.underline,
                     ),
                   ),
-                  TextSpan(
+                  const TextSpan(
                     text: "と",
                   ),
                   TextSpan(
                     text: "プライバシーポリシー",
                     recognizer: TapGestureRecognizer()..onTap = () {},
-                    style: TextStyle(
+                    style: const TextStyle(
                         color: Colors.blue,
                         fontWeight: FontWeight.w600,
                         decoration: TextDecoration.underline),
                   ),
-                  TextSpan(
+                  const TextSpan(
                     text: "に同意したものとみなされます。",
                   ),
                 ],
