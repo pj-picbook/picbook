d71005c96742cc20aa34817e4aa16e716cf3971e
yama <96728192+karokita@users.noreply.github.com>
2022-07-31 23:48:42 +0900

    背景色/Appbar/ボトムナビのカラーを設定

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
+        scaffoldBackgroundColor: HexColor('F8F5EE'),
+        appBarTheme: AppBarTheme(
+          backgroundColor: HexColor('F8F5EE'),
+          titleTextStyle: TextStyle(
+              color: HexColor('410000'),
+              fontSize: 22,
+              fontWeight: FontWeight.bold),
+          elevation: 0.0,
+        ),
       ),
       home: const BottomNavigationPage(),
     );
   }
+}
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
