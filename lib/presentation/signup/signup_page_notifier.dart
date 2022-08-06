22aa4151839078fb80180398d6517a20f8cba4f0
まさたか <shinonome419@icloud.com>
2022-08-06 23:26:46 +0900

    update: SignUpPageNotifierの実装
    ・MVVM + Repositoryというアーキテクチャのため、SignUpPageNotifierを実装。

---
+++ lib/presentation/signup/signup_page_notifier.dart
@@ -0,0 +1,17 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:picbook/domain/entity/user.dart';
+import 'package:picbook/infrastructure/user_repository.dart';
+
+final signUpPageNotifierProvider =
+    StateNotifierProvider<SignUpPageNotifier, User>((ref) {
+  return SignUpPageNotifier(
+    userRepository: ref.read(userRepository),
+  );
+});
+
+class SignUpPageNotifier extends StateNotifier<User> {
+  final UserRepository _userRepository;
+  SignUpPageNotifier({required UserRepository userRepository})
+      : _userRepository = userRepository,
+        super(User.initial());
+}
