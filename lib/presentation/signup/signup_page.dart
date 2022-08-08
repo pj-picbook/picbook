b08047d9e609f807ef2f3989e42f216f608559ed
まさたか <shinonome419@icloud.com>
2022-08-08 17:11:20 +0900

    add: SignUpPageのStateクラスを作成
    ・SignUpPageのStateクラスを作成。
    ・生年月日については未実装。

--- lib/presentation/signup/signup_page.dart
+++ lib/presentation/signup/signup_page.dart
@@ -327,3 +327,20 @@ class SignUpPage extends HookConsumerWidget {
     );
   }
 }
+
+@immutable
+class SignUpPageState {
+  const SignUpPageState({this.name = "", this.email = "", this.password = ""});
+
+  final String name;
+  final String email;
+  final String password;
+
+  SignUpPageState copyWith({String? name, String? email, String? password}) {
+    return SignUpPageState(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
+}
