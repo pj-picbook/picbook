2727471ad784774c4d2e85b74294a01a2a367ffc
yama <96728192+karokita@users.noreply.github.com>
2022-08-11 02:23:38 +0900

    マイページの画面UI

--- lib/presentation/mypage/mypage.dart
+++ lib/presentation/mypage/mypage.dart
@@ -39,39 +39,190 @@ class MyPage extends HookConsumerWidget {
           children: <Widget>[
             Flexible(
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(180),
+                borderRadius: BorderRadius.circular(150),
                 child: Image.network(
-                  'https://pbs.twimg.com/profile_images/1416944157642297344/uGjtCwxf_200x200.jpg',
-                  width: 180,
-                  height: 180,
+                  'https://i.pinimg.com/236x/ef/78/70/ef7870383f8475f8ce487fc9d19bc6fb---orange-illustration.jpg',
+                  width: 150,
+                  height: 150,
                 ),
               ),
             ),
             const SizedBox(
               width: 48,
+              height: 20,
             ),
-            Row(
-              children: <Widget>[const Text('なまえ'), Text(state.name)],
-            ),
-            Row(
+            Column(
               children: <Widget>[
-                const Text('よんだえほん'),
-                Text('${dummyUser.bookshelfs.length}さつ')
+                Text(state.name,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    )),
               ],
             ),
-            ElevatedButton(
-                onPressed: () => {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute<MyPage>(
-                          builder: (BuildContext context) => const FirstPage(),
-                        ),
-                      )
-                    },
-                child: const Text('FirstPage'))
+            const SizedBox(
+              height: 25,
+            ),
+            Divider(
+              color: HexColor('DBCCC4'),
+              thickness: 1,
+              height: 0,
+              indent: 0,
+              endIndent: 0,
+            ),
+            SizedBox(
+              height: 75,
+              child: Row(
+                children: <Widget>[
+                  const SizedBox(
+                    width: 50,
+                    child: Icon(
+                      Icons.auto_stories,
+                      color: Colors.pink,
+                      size: 24.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                  ),
+                  SizedBox(width: 110, child: const Text('よんだえほん')),
+                  SizedBox(
+                    width: 30,
+                    child: Text(
+                      '${dummyUser.bookshelfs.length}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Text('さつ'),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Divider(
+              color: HexColor('DBCCC4'),
+              thickness: 1,
+              height: 0,
+              indent: 0,
+              endIndent: 0,
+            ),
+            SizedBox(
+              height: 75,
+              child: Row(
+                children: const <Widget>[
+                  SizedBox(
+                    width: 50,
+                    child: Icon(
+                      Icons.cake,
+                      color: Colors.pink,
+                      size: 24.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                  ),
+                  SizedBox(width: 110, child: Text('ねんれい')),
+                  SizedBox(
+                    width: 30,
+                    child: Text(
+                      '5',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Text('さい'),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Divider(
+              color: HexColor('DBCCC4'),
+              thickness: 1,
+              height: 0,
+              indent: 0,
+              endIndent: 0,
+            ),
+            SizedBox(
+              height: 75,
+              child: Row(
+                children: const <Widget>[
+                  SizedBox(
+                    width: 50,
+                    child: Icon(
+                      Icons.calendar_month,
+                      color: Colors.pink,
+                      size: 24.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    ),
+                  ),
+                  SizedBox(width: 110, child: Text('とうろくび')),
+                  Text(
+                    '1000/00/00',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Divider(
+              color: HexColor('DBCCC4'),
+              thickness: 1,
+              height: 0,
+              indent: 0,
+              endIndent: 0,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<MyPage>(
+                    builder: (BuildContext context) => const FirstPage(),
+                  ),
+                )
+              },
+              child: Container(
+                height: 50,
+                width: 270,
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  border: Border.all(color: Colors.red, width: 0.5),
+                ),
+                child: const Center(
+                  child: Text(
+                    "ログアウトする",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF$hexColor';
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
