3b9740747873bbc7808a28ac30caf8d0c97a0428
まさたか <shinonome419@icloud.com>
2022-08-11 17:55:26 +0900

    update: InputNotifierを修正
    ・InputNotifierにReaderを追加し、コンストラクタとメソッドを修正。_readを使ってAuthRepositoryにアクセスできるようになった。
    ・inputProviderの引数を修正。また今後のことを考えて、autoDisposeするように修正。
    
    また、
    ・不要だったUserInputStateを削除。
    ・getIt(locator)に関するファイル、メソッド、パッケージの導入を削除。

--- lib/infrastructure/locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'auth_repository.dart';
-
-GetIt getIt = GetIt.instance;
-
-void setUpLocator() {
-  //getIt.registerLazySingleton(() => AuthRepository());
-}
--- lib/state/user_input_state.dart
+++ lib/state/user_input_state.dart
@@ -1,38 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:picbook/infrastructure/auth_repository.dart';
-import 'package:picbook/infrastructure/locator.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final inputProvider = StateNotifierProvider((ref) {
-  return InputNotifier();
+final inputProvider = StateNotifierProvider.autoDispose((ref) {
+  return InputNotifier(ref.read);
 });
 
-@immutable
-class UserInputState {
-  const UserInputState({required this.userInfo});
-  final String userInfo;
-
-  UserInputState copyWith({String? userInfo}) {
-    return UserInputState(userInfo: userInfo ?? this.userInfo);
-  }
-}
-
-class InputNotifier extends StateNotifier {
-  InputNotifier() : super([]);
-
-  final AuthRepository _repository = getIt<AuthRepository>();
+class InputNotifier extends StateNotifier<User?> {
+  final Reader _read;
+  InputNotifier(this._read) : super(null);
 
   Future<void> logIn(String email, String password) async {
     try {
-      await _repository.logIn(email, password);
+      await _read(authRepositoryProvider).logIn(email, password);
     } catch (e) {
       throw e.toString();
     }
   }
 
   Future<void> signUp(String email, String password) async {
-    await _repository.signUp(email, password);
+    await _read(authRepositoryProvider).signUp(email, password);
   }
 }
