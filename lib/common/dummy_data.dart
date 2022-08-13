9634bb5f1ec64e7faa09a4105b8db7fcc1b5a7e6
tokku5552 <physics.0121@gmail.com>
2022-08-14 08:35:01 +0900

    fix: unit testを仕様に従って修正

--- lib/common/dummy_data.dart
+++ lib/common/dummy_data.dart
@@ -1,7 +1,45 @@
+import 'package:picbook/domain/entity/book.dart';
+import 'package:picbook/domain/entity/bookshelf.dart';
+import 'package:picbook/domain/entity/bookshelf_history.dart';
 import 'package:picbook/domain/entity/user.dart';
 
 final User dummyUser = User(
   id: '0001',
   email: 'picbook@example.com',
   linkedAccount: 'Google',
+);
+final Book dummyBook = Book(title: 'book_title_1');
+
+final List<Book> dummyBooks = [
+  Book(title: 'book_title_1'),
+  Book(title: 'book_title_2'),
+  Book(title: 'book_title_3'),
+];
+
+final BookshelfHistory dummyBookshelfHistory = BookshelfHistory(
+  date: DateTime.now(),
+  book: dummyBook,
+);
+
+final List<BookshelfHistory> dummyBookshelfHistories = [
+  BookshelfHistory(
+    date: DateTime.now(),
+    book: dummyBooks[0],
+  ),
+  BookshelfHistory(
+    date: DateTime.now(),
+    book: dummyBooks[1],
+  ),
+  BookshelfHistory(
+    date: DateTime.now(),
+    book: dummyBooks[2],
+  ),
+];
+
+final Bookshelf dummyBookshelf = Bookshelf(
+  id: 'bookshelf_id',
+  owner: 'owner',
+  ownerBirthday: DateTime.now(),
+  books: dummyBooks,
+  created: DateTime.now(),
 );
--- test/presentation/mypage/mypage_notifier_test.dart
+++ test/presentation/mypage/mypage_notifier_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:picbook/common/dummy_data.dart';
+import 'package:picbook/infrastructure/auth_repository.dart';
+import 'package:picbook/infrastructure/bookshelf_repository.dart';
 import 'package:picbook/infrastructure/user_repository.dart';
 import 'package:picbook/presentation/mypage/mypage_notifier.dart';
 import 'package:mockito/mockito.dart';
@@ -10,18 +12,28 @@ import '../../mock/container.mocks.dart';
 
 void main() {
   late ProviderContainer container;
-  late MockUserRepository repository;
+  late MockUserRepository userRepository;
+  late MockAuthRepository authRepository;
+  late MockBookshelfRepository bookshelfRepository;
 
   setUp(() {
     container = overrideUserRepository();
-    repository = container.read(userRepositoryProvider) as MockUserRepository;
+    userRepository =
+        container.read(userRepositoryProvider) as MockUserRepository;
+    authRepository =
+        container.read(authRepositoryProvider) as MockAuthRepository;
+    bookshelfRepository =
+        container.read(bookshelfRepositoryProvider) as MockBookshelfRepository;
   });
 
   group('fetch', () {
     test('存在するユーザーIDを渡した際に、正しくユーザー情報を取得できること', () async {
       // ユーザー情報をmockにセット
-      when(repository.findById(id: 'test'))
+      when(authRepository.getUid()).thenAnswer((_) => 'test_uid');
+      when(userRepository.findById(id: 'test_uid'))
           .thenAnswer((_) => Future.value(dummyUser));
+      when(bookshelfRepository.fetchAll(uid: 'test_uid'))
+          .thenAnswer((realInvocation) => Future.value([dummyBookshelf]));
 
       // containerからnotifierを呼び出し
       final notifier = container.read(myPageNotifierProvider.notifier);
@@ -30,7 +42,8 @@ void main() {
       await notifier.fetch();
       final state = container.read(myPageNotifierProvider);
 
-      expect(state, dummyUser);
+      expect(state.user, dummyUser);
+      expect(state.currentBookshelf, dummyBookshelf);
     });
   });
 }
