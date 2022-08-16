3513d35b3d9d142a50128c440c66f238a9b093f0
Romu1273x <romu1273x@yahoo.co.jp>
2022-08-16 22:25:21 +0900

    bookProviderを削除

--- lib/state/book_notifier.dart
+++ lib/state/book_notifier.dart
@@ -1,17 +1,13 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:picbook/domain/entity/book.dart';
 
-final bookProvider = Provider((ref) => Book());
-
 final bookNotifierProvider = StateNotifierProvider<BookNotifier, Book>((ref) {
-  return BookNotifier(
-    bookProvider: ref.read(bookProvider),
-  );
+  return BookNotifier();
 });
 
 /// 検索画面、絵本の棚画面で更新。本棚詳細画面で参照
 class BookNotifier extends StateNotifier<Book> {
-  BookNotifier({required Book bookProvider}) : super(Book.initial());
+  BookNotifier() : super(Book.initial());
 
   void set(Book newBook) {
     state = newBook;
