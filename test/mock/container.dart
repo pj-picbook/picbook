a67cd9b8a1eea5bef88c10ac1e73a39a3fd0b1a5
tokku5552 <physics.0121@gmail.com>
2022-08-20 22:03:59 +0900

    fix: my_pageのテストにanalyticsのmockを追加

--- test/mock/container.dart
+++ test/mock/container.dart
@@ -2,8 +2,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
 import 'package:mockito/annotations.dart';
 import 'package:picbook/common/logger_provider.dart';
+import 'package:picbook/infrastructure/analytics_service.dart';
 import 'package:picbook/infrastructure/auth_repository.dart';
 import 'package:picbook/infrastructure/bookshelf_repository.dart';
+import 'package:picbook/infrastructure/provider/analytics_provider.dart';
 import 'package:picbook/infrastructure/rakuten_book_repository.dart';
 import 'package:picbook/infrastructure/user_repository.dart';
 
@@ -11,12 +13,14 @@ import 'container.mocks.dart';
 
 @GenerateMocks([
   Logger,
+  AnalyticsService,
   UserRepository,
   AuthRepository,
   BookshelfRepository,
   RakutenBookRepository
 ])
 final mockLogger = MockLogger();
+final mockAnalyticsService = MockAnalyticsService();
 final mockUserRepository = MockUserRepository();
 final mockAuthRepository = MockAuthRepository();
 final mockBookshelfRepository = MockBookshelfRepository();
@@ -27,6 +31,8 @@ ProviderContainer overrideUserRepository() {
     loggerProvider.overrideWithProvider(
       Provider(((ref) => mockLogger)),
     ),
+    analyticsServiceProvider
+        .overrideWithProvider(Provider((ref) => mockAnalyticsService)),
     userRepositoryProvider.overrideWithProvider(
       Provider((ref) => mockUserRepository),
     ),
