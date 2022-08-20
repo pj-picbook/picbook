b0206df69bc73c08a77126716bc54c12955004e1
tokku5552 <physics.0121@gmail.com>
2022-08-20 21:16:43 +0900

    add: buttonイベントの送信処理を追加

--- lib/infrastructure/analytics_service.dart
+++ lib/infrastructure/analytics_service.dart
@@ -9,6 +9,15 @@ final analyticsServiceProvider = Provider(
 
 enum AnalyticsEvent { button }
 
+extension AnalyticsEventExt on AnalyticsEvent {
+  String get name {
+    switch (this) {
+      case AnalyticsEvent.button:
+        return 'button';
+    }
+  }
+}
+
 class AnalyticsService {
   final AnalyticsProvider _provider;
   AnalyticsService({required AnalyticsProvider analyticsProvider})
@@ -16,17 +25,16 @@ class AnalyticsService {
 
   /// ボタンタップイベント送信
   Future<void> sendButtonEvent({required String buttonName}) async {
-    sendEvent(
+    _sendEvent(
         event: AnalyticsEvent.button, parameterMap: {'buttonName': buttonName});
   }
 
   /// イベントを送信する
   /// [event] AnalyticsEvent
   /// [parameterMap] パラメータMap
-  Future<void> sendEvent(
+  Future<void> _sendEvent(
       {required AnalyticsEvent event,
       Map<String, dynamic>? parameterMap}) async {
-    final eventName = event.toString().split('.')[1];
-    _provider.analytics.logEvent(name: eventName, parameters: parameterMap);
+    _provider.analytics.logEvent(name: event.name, parameters: parameterMap);
   }
 }
