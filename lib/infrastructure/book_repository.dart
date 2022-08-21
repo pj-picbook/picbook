07b120637c905ea0a575a591c3bcabb938e6d0f0
kawado <kawadon780@gmail.com>
2022-08-21 11:07:37 +0900

    add: 本登録機能追加

---
+++ lib/infrastructure/book_repository.dart
@@ -0,0 +1,80 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:picbook/domain/entity/bookshelf.dart';
+import '../domain/entity/book.dart';
+
+final booksRepositoryProvider = Provider((ref) => BooksRepository()..init());
+
+class BooksRepository {
+  final _db = FirebaseFirestore.instance;
+  late final CollectionReference _usersRef;
+
+  void init() {
+    _usersRef = _db.collection('users');
+  }
+
+  Future<Bookshelf> findById({required String uid, required String id}) async {
+    final bookshelfRef = _usersRef.doc(uid).collection('bookshelfs');
+    final doc = await bookshelfRef.doc(id).get();
+    return Bookshelf.fromJson(_jsonFromSnapshot(doc));
+  }
+
+  Future<List<Book>> fetchAll({
+    required String uid,
+    required String bookshelfsId,
+  }) async {
+    final booksRef = _usersRef
+        .doc(uid)
+        .collection('bookshelfs')
+        .doc(bookshelfsId)
+        .collection('books');
+    final snapshot = await booksRef.get();
+    return snapshot.docs
+        .map(
+          (item) => Book.fromJson(
+            _jsonFromSnapshot(item),
+          ),
+        )
+        .toList();
+  }
+
+  Future<void> create({
+    required String uid,
+    required String bookshelfId,
+    required Book book,
+  }) async {
+    await _usersRef
+        .doc(uid)
+        .collection('bookshelfs')
+        .doc(bookshelfId)
+        .collection('books')
+        .doc()
+        .set(book.toJson());
+  }
+
+  Map<String, dynamic> _jsonFromSnapshot<T extends DocumentSnapshot>(T json) {
+    return {
+      'title': json['title'],
+      'isbn': json['isbn'],
+      'titleKana': json['titleKana'],
+      'subTitle': json['subTitle'],
+      'subTitleKana': json['subTitleKana'],
+      'seriesName': json['seriesName'],
+      'seriesNameKana': json['seriesNameKana'],
+      'contents': json['contents'],
+      'contentsKana': json['contentsKana'],
+      'author': json['author'],
+      'authorKana': json['authorKana'],
+      'publisherName': json['publisherName'],
+      'size': json['size'],
+      'itemCaption': json['itemCaption'],
+      'salesDate': json['salesDate'],
+      'itemPrice': json['itemPrice'],
+      'itemUrl': json['itemUrl'],
+      'affiliateUrl': json['affiliateUrl'],
+      'smallImageUrl': json['smallImageUrl'],
+      'mediumImageUrl': json['mediumImageUrl'],
+      'largeImageUrl': json['largeImageUrl'],
+    };
+  }
+}
--- lib/presentation/searchbook/searchbook_page.dart
+++ lib/presentation/searchbook/searchbook_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'package:picbook/infrastructure/rakuten_book_repository.dart';
 import 'package:picbook/presentation/book_detail/book_detail_page.dart';
 import 'package:picbook/state/book_notifier.dart';
 import '../../presentation/searchbook/searchbook_page_notifier.dart';
 import '../widget/book_box.dart';
+import '../widget/dialog.dart';
 import '../barcode_scanner_page/barcode_scanner_page.dart';
+import '../../infrastructure/bookshelf_repository.dart';
+import '../../infrastructure/book_repository.dart';
+import '../../infrastructure/auth_repository.dart';
 
 class SearchBookPage extends HookConsumerWidget {
   const SearchBookPage({Key? key}) : super(key: key);
@@ -17,6 +22,10 @@ class SearchBookPage extends HookConsumerWidget {
     final notifier = ref.watch(rakutenBookPageNotifierProvider.notifier);
     final textFieldController = useTextEditingController();
     final bookNotifier = ref.watch(bookNotifierProvider.notifier);
+
+    final bookshelfNotifier = ref.watch(bookshelfRepositoryProvider);
+    final authNotifier = ref.watch(authRepositoryProvider);
+    final booksNotifier = ref.watch(booksRepositoryProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -61,6 +70,18 @@ class SearchBookPage extends HookConsumerWidget {
                   final item = state.books[index];
                   return InkWell(
                       child: BookBox(
+                        onPressed: () async {
+                          final bookshelfs = await bookshelfNotifier.fetchAll(
+                              uid: authNotifier.getUid()!);
+                          booksNotifier.create(
+                            uid: authNotifier.getUid()!,
+                            bookshelfId: bookshelfs[0].id, // TODO:本来は複数予定
+                            book: item,
+                          );
+
+                          showAlertDialog(ref,
+                              title: '本の追加', content: '本の追加が完了しました');
+                        },
                         book: item,
                       ),
                       onTap: () {
