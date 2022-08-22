207fa7bfa6e607fc5231df995a89b4eb77da3ba3
kawado <kawadon780@gmail.com>
2022-08-22 22:08:50 +0900

    fix: Notifierを使用する様に修正

--- lib/presentation/searchbook/searchbook_page.dart
+++ lib/presentation/searchbook/searchbook_page.dart
@@ -4,14 +4,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:picbook/infrastructure/rakuten_book_repository.dart';
 import 'package:picbook/presentation/book_detail/book_detail_page.dart';
-import 'package:picbook/state/book_notifier.dart';
 import '../../presentation/searchbook/searchbook_page_notifier.dart';
 import '../widget/book_box.dart';
 import '../widget/dialog.dart';
 import '../barcode_scanner_page/barcode_scanner_page.dart';
-import '../../infrastructure/bookshelf_repository.dart';
-import '../../infrastructure/book_repository.dart';
-import '../../infrastructure/auth_repository.dart';
 
 class SearchBookPage extends HookConsumerWidget {
   const SearchBookPage({Key? key}) : super(key: key);
@@ -21,11 +17,6 @@ class SearchBookPage extends HookConsumerWidget {
     final state = ref.watch(rakutenBookPageNotifierProvider);
     final notifier = ref.watch(rakutenBookPageNotifierProvider.notifier);
     final textFieldController = useTextEditingController();
-    final bookNotifier = ref.watch(bookNotifierProvider.notifier);
-
-    final bookshelfNotifier = ref.watch(bookshelfRepositoryProvider);
-    final authNotifier = ref.watch(authRepositoryProvider);
-    final booksNotifier = ref.watch(booksRepositoryProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -71,21 +62,13 @@ class SearchBookPage extends HookConsumerWidget {
                   return InkWell(
                       child: BookBox(
                         onPressed: () async {
-                          final bookshelfs = await bookshelfNotifier.fetchAll(
-                              uid: authNotifier.getUid()!);
-                          booksNotifier.create(
-                            uid: authNotifier.getUid()!,
-                            bookshelfId: bookshelfs[0].id, // TODO:本来は複数予定
-                            book: item,
-                          );
-
+                          notifier.registerBook(book: item);
                           showAlertDialog(ref,
                               title: '本の追加', content: '本の追加が完了しました');
                         },
                         book: item,
                       ),
                       onTap: () {
-                        bookNotifier.set(item);
                         Navigator.of(context).push(
                           MaterialPageRoute(
                               builder: (context) => const BookDetailPage()),
--- lib/presentation/searchbook/searchbook_page_notifier.dart
+++ lib/presentation/searchbook/searchbook_page_notifier.dart
@@ -1,21 +1,39 @@
+import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../domain/entity/book.dart';
 import '../../infrastructure/rakuten_book_repository.dart';
+import '../../infrastructure/bookshelf_repository.dart';
+import '../../infrastructure/book_repository.dart';
+import '../../infrastructure/auth_repository.dart';
 import '../../state/search_book_state.dart';
 
 final rakutenBookPageNotifierProvider =
     StateNotifierProvider<SearchBookPageNotifier, SearchBookState>((ref) {
   return SearchBookPageNotifier(
     rakutenBookRepository: ref.read(rakutenBookRepositoryProvider),
+    bookshelfRepository: ref.read(bookshelfRepositoryProvider),
+    booksRepository: ref.read(booksRepositoryProvider),
+    baseAuthRepository: ref.read(authRepositoryProvider),
   );
 });
 
 ///
 class SearchBookPageNotifier extends StateNotifier<SearchBookState> {
-  SearchBookPageNotifier({required RakutenBookRepository rakutenBookRepository})
-      : _rakutenBookRepository = rakutenBookRepository,
+  SearchBookPageNotifier({
+    required RakutenBookRepository rakutenBookRepository,
+    required BookshelfRepository bookshelfRepository,
+    required BooksRepository booksRepository,
+    required BaseAuthRepository baseAuthRepository,
+  })  : _rakutenBookRepository = rakutenBookRepository,
+        _bookshelfRepository = bookshelfRepository,
+        _booksRepository = booksRepository,
+        _baseAuthRepository = baseAuthRepository,
         super(SearchBookState(books: []));
 
   final RakutenBookRepository _rakutenBookRepository;
+  final BookshelfRepository _bookshelfRepository;
+  final BooksRepository _booksRepository;
+  final BaseAuthRepository _baseAuthRepository;
 
   Future<void> fetch({
     required SearchType searchType,
@@ -24,6 +42,18 @@ class SearchBookPageNotifier extends StateNotifier<SearchBookState> {
     state = await _rakutenBookRepository.search(
       searchType: searchType,
       keyWord: keyWord,
+    );
+  }
+
+  Future<void> registerBook({
+    required Book book,
+  }) async {
+    final bookshelfs =
+        await _bookshelfRepository.fetchAll(uid: _baseAuthRepository.getUid()!);
+    _booksRepository.create(
+      uid: _baseAuthRepository.getUid()!,
+      bookshelfId: bookshelfs[0].id, // TODO:本来は複数予定
+      book: book,
     );
   }
 }
