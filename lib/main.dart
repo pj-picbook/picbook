012bb636f55fa637f2549c06e3884c44989776af
まさたか <shinonome419@icloud.com>
2022-08-24 16:25:20 +0900

    add: クラッシュハンドラの追加、およびテストクラッシュの確認
    ・main.dartに、全てのエラーをキャッチするクラッシュハンドラを追加
    ・TextButtonを追加して初のテストクラッシュを送信(追加したTextButtonは削除済み)
    ・iosのCrashlyticsコンソールでテストクラッシュを確認

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,6 +13,9 @@ import 'common/logger_provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  //Flutterフレームワーク内でスローされた全てのエラーを自動的にキャッチする、クラッシュハンドラ
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
   runApp(
     const ProviderScope(
