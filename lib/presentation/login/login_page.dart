8f21f7925814f58c2e24011f21a373e29899198b
karokita <ui.yamanaka@gmail.com>
2022-08-28 17:41:01 +0900

    ・新規登録、ログイン・ファーストページの色を変更 ・ログインページのパスワード忘れのリンク先をgoogleフォームに変更 ・作品追加時のダイアログカラーと文言を変更 ・バーコードスキャンページの文言を変更

--- lib/presentation/login/login_page.dart
+++ lib/presentation/login/login_page.dart
@@ -5,7 +5,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:picbook/common/logger_provider.dart';
 import 'package:picbook/main.dart';
 import 'package:picbook/presentation/login/login_notifier.dart';
-
 import '../agreement_page/agreement_page.dart';
 
 class LogInPage extends HookConsumerWidget {
@@ -21,13 +20,13 @@ class LogInPage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text(
           "ログイン",
-          style: TextStyle(fontSize: 17.0, color: Colors.white),
         ),
-        backgroundColor: Colors.blue[900],
+        iconTheme: const IconThemeData(
+          color: Colors.brown, //change your color here
+        ),
       ),
       body: Form(
         child: Container(
-          color: Colors.grey[300],
           padding: const EdgeInsets.only(left: 40.0, right: 40.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -36,7 +35,7 @@ class LogInPage extends HookConsumerWidget {
                 alignment: Alignment.centerLeft,
                 child: Text(
                   "メールアドレス",
-                  style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
                 ),
               ),
               Container(
@@ -44,7 +43,7 @@ class LogInPage extends HookConsumerWidget {
                 width: 300,
                 decoration: BoxDecoration(
                   color: Colors.white70,
-                  border: Border.all(color: Colors.red, width: 0.5),
+                  border: Border.all(color: Colors.brown, width: 0.5),
                 ),
                 child: TextFormField(
                   controller: emailController,
@@ -53,8 +52,6 @@ class LogInPage extends HookConsumerWidget {
                   textAlign: TextAlign.center,
                   decoration: const InputDecoration(
                     border: InputBorder.none,
-                    hintText: "xxxxxx@xxxxxxxxxxx",
-                    hintStyle: TextStyle(fontSize: 13.0),
                   ),
                 ),
               ),
@@ -65,7 +62,7 @@ class LogInPage extends HookConsumerWidget {
                 alignment: Alignment.centerLeft,
                 child: Text(
                   "パスワード",
-                  style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
                 ),
               ),
               Container(
@@ -73,7 +70,7 @@ class LogInPage extends HookConsumerWidget {
                 width: 300,
                 decoration: BoxDecoration(
                   color: Colors.white70,
-                  border: Border.all(color: Colors.red, width: 0.5),
+                  border: Border.all(color: Colors.brown, width: 0.5),
                 ),
                 child: TextFormField(
                   controller: passwordController,
@@ -82,22 +79,13 @@ class LogInPage extends HookConsumerWidget {
                   textAlign: TextAlign.center,
                   decoration: const InputDecoration(
                     border: InputBorder.none,
-                    hintText: "………………………",
-                    hintStyle: TextStyle(fontSize: 13.0),
                   ),
                 ),
               ),
               const SizedBox(
-                height: 40.0,
+                height: 60.0,
               ),
-              Container(
-                height: 50,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.white70,
-                  border: Border.all(color: Colors.red, width: 0.5),
-                ),
-                child: TextButton(
+              ElevatedButton(
                   onPressed: () async {
                     try {
                       await notifier.logIn();
@@ -111,16 +99,21 @@ class LogInPage extends HookConsumerWidget {
                       logger.e(e);
                     }
                   },
-                  child: const Text(
-                    "ログインする",
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w900,
+                  child: Container(
+                    height: 50,
+                    width: 270,
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("ログイン",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w100,
+                              fontSize: 16,
+                            )),
+                      ],
                     ),
-                  ),
-                ),
-              ),
+                  )),
               const SizedBox(
                 height: 20.0,
               ),
@@ -142,21 +135,24 @@ class LogInPage extends HookConsumerWidget {
                             context,
                             MaterialPageRoute(
                               builder: (context) => const AgreementPage(
-                                title: "利用規約",
+                                title: "お問い合わせフォーム",
                                 url:
-                                    "https://pj-picbook.github.io/picdoc/docs/terms.html",
+                                    "https://docs.google.com/forms/d/e/1FAIpQLSc1mHvSYh_bMLD6j5lZsBAdbP-jdViXPrJWbX_CSBMPZNbD0A/viewform",
                               ),
                             ),
                           );
                         },
-                      style: const TextStyle(
-                        color: Colors.blue,
+                      style: TextStyle(
+                        color: Colors.deepOrange.shade700,
                         fontWeight: FontWeight.w600,
                         decoration: TextDecoration.underline,
                       ),
                     ),
                   ],
                 ),
+              ),
+              const SizedBox(
+                height: 50.0,
               ),
             ],
           ),
