e22ea1a89bd3b81604a3ed47ab9dc223211c1f80
まさたか <shinonome419@icloud.com>
2022-08-29 17:29:28 +0900

    update: ログインボタンを押下時、エラー文をsnackBarで表示するよう変更
    会員登録ボタンと同様、ログインボタンでもエラー文をsnackBarで表示するよう変更した。

--- lib/presentation/login/login_page.dart
+++ lib/presentation/login/login_page.dart
@@ -109,6 +109,8 @@ class LogInPage extends HookConsumerWidget {
                           (route) => false))();
                     } catch (e) {
                       logger.e(e);
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(e.toString())));
                     }
                   },
                   child: const Text(
