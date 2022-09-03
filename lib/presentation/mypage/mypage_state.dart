6efeba08d433ab515b36f044697c21555a2a9075
Romu1273x <romu1273x@yahoo.co.jp>
2022-09-04 01:33:56 +0900

    flutter analyze & testエラー対応

--- lib/presentation/mypage/mypage_state.dart
+++ lib/presentation/mypage/mypage_state.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:flutter/foundation.dart';
 import 'package:picbook/domain/entity/book.dart';
 import 'package:picbook/domain/entity/bookshelf.dart';
 import 'package:picbook/domain/entity/user.dart';
