467263f936a44093a055012076eca9970c476921
まさたか <shinonome419@icloud.com>
2022-09-07 22:15:50 +0900

    add: AccountPageStateを追加
    AccountPageStateを追加。またfreezedのbuild_runnerを実行しfreezedファイルも作成。

---
+++ lib/state/account_page_state.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:picbook/domain/entity/user.dart';
+import 'package:flutter/foundation.dart';
+
+part 'account_page_state.freezed.dart';
+
+@freezed
+class AccountPageState with _$AccountPageState {
+  factory AccountPageState({
+    required User user,
+  }) = _AccountPageState;
+
+  factory AccountPageState.initial() => AccountPageState(user: User.initial());
+}
