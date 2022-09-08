5c2011e0a4192029f959aee8af768927f8c4b110
まさたか <shinonome419@icloud.com>
2022-09-08 21:24:34 +0900

    update: AccountPageを変更
    AccountPageをStatelessWidgetからHookConsumerWidgetへ変更。
    また試しに「メールアドレス」欄にユーザーidを表示。
    またダイアログの「削除する」ボタンにdeleteUser()メソッドを追加。

--- lib/presentation/account_page/account_page.dart
+++ lib/presentation/account_page/account_page.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:picbook/presentation/account_page/account_page_notifier.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends HookConsumerWidget {
   const AccountPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(accountPageNotifierProvider);
+    final notifier = ref.watch(accountPageNotifierProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -37,9 +44,9 @@ class AccountPage extends StatelessWidget {
                   fontSize: 14,
                 ),
               ),
-              trailing: const Text(
-                "email",
-                style: TextStyle(fontWeight: FontWeight.w300),
+              trailing: Text(
+                FirebaseAuth.instance.currentUser!.uid,
+                style: const TextStyle(fontWeight: FontWeight.w300),
               ),
             ),
             const SizedBox(
@@ -77,10 +84,12 @@ class AccountPage extends StatelessWidget {
                           },
                         ),
                       ),
-                      const CupertinoDialogAction(
+                      CupertinoDialogAction(
                         child: TextButton(
-                          onPressed: null,
-                          child: Text(
+                          onPressed: () async {
+                            notifier.deleteUser();
+                          },
+                          child: const Text(
                             "削除する",
                             style: TextStyle(
                                 color: Colors.black,
