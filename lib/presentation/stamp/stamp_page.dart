dec6350f7e3e6c6724a75be8559fce1daab682ac
Romu1273x <romu1273x@yahoo.co.jp>
2022-09-11 23:11:24 +0900

    ボトムナビにスタンプ画面を追加

---
+++ lib/presentation/stamp/stamp_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class StampPage extends HookConsumerWidget {
+  const StampPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
