ad5b1c9314b3110c4a715e60e4dc2d4456b39475
まさたか <shinonome419@icloud.com>
2022-09-15 06:48:18 +0900

    add: deleteUser()とreAuth()メソッドを追加
    ユーザの再認証とユーザ削除に必要なメソッドをAuthRepositoryに追加した。

--- lib/infrastructure/auth_repository.dart
+++ lib/infrastructure/auth_repository.dart
@@ -18,7 +18,8 @@ abstract class BaseAuthRepository {
   String? getUid();
   Stream<User?> get authStateChange;
   Future<void> logOut();
-  Future<void> deleteUser();
+  Future<void> deleteUser({required String password});
+  Future<void> reAuth({required String password});
 }
 
 class AuthRepository implements BaseAuthRepository {
@@ -71,11 +72,23 @@ class AuthRepository implements BaseAuthRepository {
   }
 
   @override
-  Future<void> deleteUser() async {
+  Future<void> deleteUser({required String password}) async {
     try {
-      await auth.currentUser?.delete();
-    } catch (e) {
-      _logger.e(e);
+      await reAuth(password: password);
+      await auth.currentUser!.delete();
+    } on FirebaseAuthException catch (e) {
+      throw convertAuthError(e.code);
+    }
+  }
+
+  @override
+  Future<void> reAuth({required String password}) async {
+    try {
+      final AuthCredential authCredential = EmailAuthProvider.credential(
+          email: auth.currentUser!.email!, password: password);
+      auth.currentUser!.reauthenticateWithCredential(authCredential);
+    } on FirebaseAuthException catch (e) {
+      throw convertAuthError(e.code);
     }
   }
 
