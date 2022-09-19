5be6b1101dd16746121a5e8ef1476064570791bf
Romu1273x <romu1273x@yahoo.co.jp>
2022-09-19 15:57:26 +0900

    カレンダーごとスクロールできるように変更

--- lib/presentation/stamp/stamp_page.dart
+++ lib/presentation/stamp/stamp_page.dart
@@ -22,120 +22,119 @@ class StampPage extends HookConsumerWidget {
     }, []);
     return Scaffold(
       appBar: AppBar(title: const Text('読んだスタンプ')),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: TableCalendar(
-              locale: 'ja_JP',
-              daysOfWeekHeight: 30,
-              rowHeight: 60,
-              headerStyle: const HeaderStyle(
-                titleCentered: true,
-                formatButtonVisible: false,
-              ),
-              calendarBuilders: CalendarBuilders(
-                dowBuilder: (context, day) {
-                  final weekDays = {
-                    1: 'げつ',
-                    2: 'か',
-                    3: 'すい',
-                    4: 'もく',
-                    5: 'きん',
-                    6: 'ど',
-                    7: 'にち',
-                  };
-                  return Container(
-                    alignment: Alignment.topCenter,
-                    child: Text(weekDays[day.weekday].toString()),
-                  );
-                },
-                defaultBuilder: (context, day, focusedDay) {
-                  return Container(
-                    alignment: Alignment.topCenter,
-                    child: Text(day.day.toString()),
-                  );
-                },
-                outsideBuilder: (context, day, focusedDay) {
-                  return Container(
-                    alignment: Alignment.topCenter,
-                    child: Text(
-                      day.day.toString(),
-                      style: TextStyle(color: Colors.black.withOpacity(0.4)),
-                    ),
-                  );
-                },
-                selectedBuilder: (context, day, focusedDay) {
-                  return Container(
-                    alignment: Alignment.topCenter,
-                    child: Text(
-                      day.day.toString(),
-                      style: const TextStyle(color: Colors.brown),
-                    ),
-                  );
-                },
-                todayBuilder: (context, day, focusedDay) {
-                  return Container(
-                    alignment: Alignment.topCenter,
-                    child: Text(day.day.toString()),
-                  );
-                },
-                markerBuilder: (context, date, event) {
-                  if (event.isNotEmpty) {
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TableCalendar(
+                locale: 'ja_JP',
+                daysOfWeekHeight: 30,
+                rowHeight: 60,
+                headerStyle: const HeaderStyle(
+                  titleCentered: true,
+                  formatButtonVisible: false,
+                ),
+                calendarBuilders: CalendarBuilders(
+                  dowBuilder: (context, day) {
+                    final weekDays = {
+                      1: 'げつ',
+                      2: 'か',
+                      3: 'すい',
+                      4: 'もく',
+                      5: 'きん',
+                      6: 'ど',
+                      7: 'にち',
+                    };
                     return Container(
-                      padding: const EdgeInsets.only(top: 5),
-                      alignment: Alignment.center,
-                      child: Icon(
-                        Icons.sentiment_very_satisfied,
-                        color: Colors.deepOrange.shade700,
-                        size: 32,
+                      alignment: Alignment.topCenter,
+                      child: Text(weekDays[day.weekday].toString()),
+                    );
+                  },
+                  defaultBuilder: (context, day, focusedDay) {
+                    return Container(
+                      alignment: Alignment.topCenter,
+                      child: Text(day.day.toString()),
+                    );
+                  },
+                  outsideBuilder: (context, day, focusedDay) {
+                    return Container(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        day.day.toString(),
+                        style: TextStyle(color: Colors.black.withOpacity(0.4)),
                       ),
                     );
-                  } else {
+                  },
+                  selectedBuilder: (context, day, focusedDay) {
                     return Container(
-                      padding: const EdgeInsets.only(top: 5),
-                      alignment: Alignment.center,
-                      child: Icon(
-                        Icons.circle,
-                        color: Colors.black.withOpacity(0.1),
-                        size: 32,
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        day.day.toString(),
+                        style: const TextStyle(color: Colors.brown),
                       ),
                     );
-                  }
+                  },
+                  todayBuilder: (context, day, focusedDay) {
+                    return Container(
+                      alignment: Alignment.topCenter,
+                      child: Text(day.day.toString()),
+                    );
+                  },
+                  markerBuilder: (context, date, event) {
+                    if (event.isNotEmpty) {
+                      return Container(
+                        padding: const EdgeInsets.only(top: 5),
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.sentiment_very_satisfied,
+                          color: Colors.deepOrange.shade700,
+                          size: 32,
+                        ),
+                      );
+                    } else {
+                      return Container(
+                        padding: const EdgeInsets.only(top: 5),
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.circle,
+                          color: Colors.black.withOpacity(0.1),
+                          size: 32,
+                        ),
+                      );
+                    }
+                  },
+                ),
+                firstDay: DateTime.utc(2020, 1, 1),
+                lastDay: DateTime.utc(2040, 1, 1),
+                focusedDay: state.focusedDay!,
+                onDaySelected: (selectedDay, focusedDay) {
+                  notifier.onDaySelected(
+                    selectedDay: selectedDay,
+                    focusedDay: focusedDay,
+                    books: state.events?[selectedDay] ?? [],
+                  );
+                },
+                selectedDayPredicate: (day) {
+                  return isSameDay(state.selectedDay, day);
+                },
+                eventLoader: (date) {
+                  return state.events?[date] ?? [];
                 },
               ),
-              firstDay: DateTime.utc(2020, 1, 1),
-              lastDay: DateTime.utc(2040, 1, 1),
-              focusedDay: state.focusedDay!,
-              onDaySelected: (selectedDay, focusedDay) {
-                notifier.onDaySelected(
-                  selectedDay: selectedDay,
-                  focusedDay: focusedDay,
-                  books: state.events?[selectedDay] ?? [],
-                );
-              },
-              selectedDayPredicate: (day) {
-                return isSameDay(state.selectedDay, day);
-              },
-              eventLoader: (date) {
-                return state.events?[date] ?? [];
-              },
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: state.books?.length ?? 0,
-              itemBuilder: (context, index) {
-                return Container(
-                    padding: const EdgeInsets.all(60),
-                    child: Image.network(
-                      state.books![index].largeImageUrl!,
-                      fit: BoxFit.fill,
-                    ));
-              },
-            ),
-          ),
-        ],
+            if (state.books != null) ...[
+              for (int i = 0; i < state.books!.length; i++)
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Image.network(
+                    state.books![i].largeImageUrl!,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+            ],
+          ],
+        ),
       ),
     );
   }
