8bf4e0632b69c300736658e0ca272d42562d6659
まさたか <shinonome419@icloud.com>
2022-09-24 18:18:19 +0900

    add: カメラアイコンを追加
    マイページのユーザアイコンに重ねるかたちで、カメラアイコンを追加した。
    まだ機能は実装していないが、ジェスチャディテクターでラップしており、タップすることでCloud Storageとやり取りできるようにする。

--- lib/presentation/mypage/mypage.dart
+++ lib/presentation/mypage/mypage.dart
@@ -51,12 +51,27 @@ class MyPage extends HookConsumerWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(150),
-                  child: Image.network(
-                    'https://i.pinimg.com/236x/ef/78/70/ef7870383f8475f8ce487fc9d19bc6fb---orange-illustration.jpg',
-                    width: 100,
-                    height: 100,
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(150),
+                        child: Image.network(
+                          'https://i.pinimg.com/236x/ef/78/70/ef7870383f8475f8ce487fc9d19bc6fb---orange-illustration.jpg',
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: null,
+                        child: const Align(
+                          alignment: Alignment.bottomRight,
+                          child: CircleAvatar(
+                            child: Icon(Icons.photo_camera),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 const SizedBox(
