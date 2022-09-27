7419e72984ff3dfcd27236d05682e11b110aacab
まさたか <shinonome419@icloud.com>
2022-09-27 20:31:18 +0900

    add: imagePickerServiceProviderの追加
    ImagePickerServiceを呼び出すため、imagePickerServiceProviderを追加した。

--- lib/infrastructure/image_picker_service.dart
+++ lib/infrastructure/image_picker_service.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final imagePickerServiceProvider = Provider((ref) => ImagePickerService());
 
 class ImagePickerService {
   final imagePicker = ImagePicker();
