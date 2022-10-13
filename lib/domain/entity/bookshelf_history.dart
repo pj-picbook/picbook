533e01b087efc1f377aabdc8a93f031737955b03
karokita <ui.yamanaka@gmail.com>
2022-10-13 20:59:01 +0900

    バーコードスキャン時の文字サイズ変更

--- lib/domain/entity/bookshelf_history.dart
+++ lib/domain/entity/bookshelf_history.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:picbook/common/timstamp_converter.dart';
+// import 'package:picbook/common/timstamp_converter.dart';
 import 'package:picbook/domain/entity/book.dart';
 import 'package:picbook/common/date_time_converter.dart';
 part 'bookshelf_history.freezed.dart';
