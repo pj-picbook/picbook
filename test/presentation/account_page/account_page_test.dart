57faf2825109ffdfd8dcbbc12899e4a58be973d7
tokku5552 <physics.0121@gmail.com>
2022-10-16 17:49:28 +0900

    fix: 不要なimportを削除

--- test/presentation/account_page/account_page_test.dart
+++ test/presentation/account_page/account_page_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/mockito.dart';
-import 'package:network_image_mock/network_image_mock.dart';
 import 'package:picbook/common/logger_provider.dart';
 import 'package:picbook/infrastructure/auth_repository.dart';
 import 'package:picbook/presentation/account_page/account_page.dart';
